16519c952c2dfebdd9c358c8ddec8b826da8c1b4
Sourab19 <sourabberlin007@gmail.com>
2024-09-27 16:45:55 +0530

    Intro to React Query

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import { useQuery } from "@tanstack/react-query";
+import "./App.css";
+import axios from "axios";
 
 function App() {
+  const priceQuery = useQuery({
+    queryKey: ["price"],
+    queryFn: () => {
+      return axios(
+        "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd"
+      );
+    },
+    refetchInterval: 1000 * 5,
+  });
+
+  const customerQuery = useQuery({
+    queryKey: ["customers"],
+    queryFn: () => {
+      return axios("http://localhost:3000/api/customers");
+    },
+    refetchInterval: 1000 * 10,
+  });
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {priceQuery?.error ? <p>Error......</p> : null}
+      {priceQuery?.isLoading ? <p>Loading......</p> : null}
+      {priceQuery?.data?.data?.bitcoin?.usd
+        ? priceQuery.data.data.bitcoin.usd
+        : null}
     </div>
   );
 }
